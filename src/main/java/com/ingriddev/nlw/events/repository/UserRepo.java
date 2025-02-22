d5f7a559e6497470a09c556480163f26ebd6c7f0
ingridevv <personnel.ingrid@gmail.com>
2025-02-21 22:40:32 -0300

    User mapping

---
+++ src/main/java/com/ingriddev/nlw/events/repository/UserRepo.java
@@ -0,0 +1,7 @@
+package com.ingriddev.nlw.events.repository;
+
+import com.ingriddev.nlw.events.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends CrudRepository<User, Integer> {
+}
